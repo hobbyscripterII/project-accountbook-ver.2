16eaef3619f1920c44ea19d0a5852b7e0ecf8a7f
hobbyscripterII <wruomma2@naver.com>
2024-01-21 09:04:56 +0900

    Initial commit

---
+++ src/main/java/com/project/softaccountbook/SoftAccountBookApplication.java
@@ -0,0 +1,13 @@
+package com.project.softaccountbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftAccountBookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoftAccountBookApplication.class, args);
+    }
+
+}
