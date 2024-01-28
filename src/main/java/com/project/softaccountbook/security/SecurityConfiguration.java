3283a2c45c0abe9cca9ead1e50d572ed3935651a
hobbyscripterII <wruomma2@naver.com>
2024-01-28 23:01:07 +0900

    :sparkles: feat: security 폼 로그인 방식 적용(demo)

--- src/main/java/com/project/softaccountbook/security/SecurityConfiguration.java
+++ src/main/java/com/project/softaccountbook/security/SecurityConfiguration.java
@@ -23,22 +23,25 @@ public class SecurityConfiguration { // 스프링 컨테이너가 호출함
                 // 로그인 페이지 커스텀
                 .formLogin(f -> f // 인증 방법을 폼 로그인 방식으로 지정
                         .loginPage("/sign-in") // 로그인 페이지(html) 경로 지정
-                        .loginProcessingUrl("/process-login") // url 호출 시 security가 낚아 챔, controller에 구현할 필요 x
+                        .loginProcessingUrl("/login") // url 호출 시 security가 낚아 챔, controller에 구현할 필요 x
+                        .usernameParameter("email")
+                        .passwordParameter("upw")
                         .failureUrl("/sign-in?error") // 인증 실패 시 호출되는 url
                         .defaultSuccessUrl("/") // 인증 완료 후 호출되는 url
                         .permitAll())
+                // 인증, 인가에서 예외 발생시 커스텀 예외 처리 핸들러
                 .exceptionHandling(e -> e
                         .accessDeniedPage("/error/403")) // 예외 발생 시 접근 제한 페이지 설정
-                .authorizeHttpRequests(a -> a // 접근 권한 확인 및 접근 허용 여부 결정
+                // 접근 권한 확인 및 접근 허용 여부 결정
+                .authorizeHttpRequests(a -> a
                         .requestMatchers(
                                 "/",
                                 "/css/**", "/js/**", "/img/**", // resource
                                 "/sign-up",
                                 "/sign-in",
                                 "/sign-out",
-                                "/process-login",
                                 "/email/**", // 이메일 전송 / 이메일 인증코드 확인
-                                "/nm-chk", // 닉네임 중복 체크
+                                "/nm-chk",
                                 "/error/**"
                         ).permitAll() // 인가 필요 x
                         .anyRequest()
@@ -49,19 +52,18 @@ public class SecurityConfiguration { // 스프링 컨테이너가 호출함
 
     @Bean
     public InMemoryUserDetailsManager userDetailsManager() {
-        // db 사용 x 메모리에 회원 정보 저장
         UserDetails userDetails = User.withDefaultPasswordEncoder()
                 .username("test")
                 .password("test")
                 .roles("USER")
                 .build();
 
-        // InMemoryUserDetailsManager - db 사용 x 메모리 사용 o 테스트 환경 / 데모 환경에서 사용됨
+        // db 사용 x 메모리 사용 o 테스트 환경 / 데모 환경에서 사용됨
         return new InMemoryUserDetailsManager(userDetails);
     }
 
-    @Bean
-    public PasswordEncoder passwordEncoder() { // 비밀번호 암호화
-        return new BCryptPasswordEncoder();
-    }
+//    @Bean
+//    public PasswordEncoder passwordEncoder() { // 비밀번호 암호화
+//        return new BCryptPasswordEncoder();
+//    }
 }
